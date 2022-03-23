6fb900e736b75945b14b3f20febce047a34e74ed
stefan1202 <stefan.durla@gmail.com>
2022-03-23 19:03:10 +0200

    Initial commit

---
+++ src/test/java/ro/sda/javaremote31/SpringModule/SpringModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.sda.javaremote31.SpringModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
